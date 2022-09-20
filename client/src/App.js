97dfd083839ddc9e8d175060060a1afc2c6b6dae
Josh <josh@hessjd.com>
2022-09-20 11:43:38 -0500

    update

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,71 @@
 import './App.css';
+import React from 'react';
+import {
+	Chart as ChartJS,
+	CategoryScale,
+	LinearScale,
+	PointElement,
+	LineElement,
+	Title,
+	Tooltip,
+	Legend,
+} from 'chart.js';
+import { Line } from 'react-chartjs-2';
 
 function App() {
-	return <div className='App'></div>;
+	ChartJS.register(
+		CategoryScale,
+		LinearScale,
+		PointElement,
+		LineElement,
+		Title,
+		Tooltip,
+		Legend
+	);
+
+	const options = {
+		scales: {
+			y: {
+				beginAtZero: true,
+			},
+		},
+	};
+
+	const data = {
+		labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+		datasets: [
+			{
+				label: '# of Votes',
+				data: [12, 19, 3, 5, 2, 3],
+				backgroundColor: [
+					'rgba(255, 99, 132, 0.2)',
+					'rgba(54, 162, 235, 0.2)',
+					'rgba(255, 206, 86, 0.2)',
+					'rgba(75, 192, 192, 0.2)',
+					'rgba(153, 102, 255, 0.2)',
+					'rgba(255, 159, 64, 0.2)',
+				],
+				borderColor: [
+					'rgba(255, 99, 132, 1)',
+					'rgba(54, 162, 235, 1)',
+					'rgba(255, 206, 86, 1)',
+					'rgba(75, 192, 192, 1)',
+					'rgba(153, 102, 255, 1)',
+					'rgba(255, 159, 64, 1)',
+				],
+				borderWidth: 1,
+			},
+		],
+	};
+
+	return (
+		<div className='App'>
+			<Line
+				data={data}
+				options={options}
+			/>
+		</div>
+	);
 }
 
 export default App;
