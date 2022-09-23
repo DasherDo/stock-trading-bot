50af8b425af6db920a646acc9c36d9f0cee33c6f
Josh <josh@hessjd.com>
2022-09-23 17:03:41 -0500

    update

--- server/WebScraper.js
+++ server/WebScraper.js
@@ -1,19 +1,20 @@
 require('dotenv').config();
-const axios = require('axios');
-const cheerio = require('cheerio');
 const puppeteer = require('puppeteer');
 
 const AAPL = process.env.AAPL;
 const TSLA = process.env.TSLA;
+const URL = process.env.URL;
 const INTERVAL = 60 * 1000;
 let stocks = {};
 
-const initialValues = async (url, symbol) => {
+const initialValues = async (symbol) => {
+	console.log(`Working on ${symbol}`);
 	const browser = await puppeteer.launch();
 	const page = await browser.newPage();
-
+	const fullURL = `${URL}/${symbol}?p=${symbol}`;
+	const pastURL = `${URL}/${symbol}/history?p=${symbol}`;
 	try {
-		await page.goto(url, { timeout: 180000 });
+		await page.goto(fullURL, { timeout: 180000 });
 		const prevClosePriceElement = await page.waitForXPath(
 			'//*[@id="quote-summary"]/div[1]/table/tbody/tr[1]/td[2]'
 		);
@@ -35,18 +36,34 @@ const initialValues = async (url, symbol) => {
 			(element) => element.textContent,
 			priceElement
 		);
+		await page.goto(pastURL, { timeout: 180000 });
+		const d5 = [];
+		// const dElement = await page.waitForXPath(
+		// 	`//*[@id="Col1-1-HistoricalDataTable-Proxy"]/section/div[2]/table/tbody/tr[${i}]/td[5]`
+		// );
+		const d = await page.evaluate(
+			(element) => element.textContent,
+			dElement
+		);
+		console.log(d);
 		const stock = {
 			price: price,
 			prevClosePrice: prevClosePrice,
 			openPrice: openPrice,
+			d5: d5,
 		};
 		stocks = { ...stocks, [symbol]: stock };
-		console.log(stocks);
 		return false;
 	} catch (err) {
 		console.log(err);
 	}
 };
 
-initialValues(AAPL, 'AAPL');
-initialValues(TSLA, 'TSLA');
+const getStocks = async (...args) => {
+	for (const url of args) {
+		await initialValues(url);
+	}
+	console.log(stocks);
+};
+
+getStocks('AAPL', 'TSLA', 'GOOGL');
