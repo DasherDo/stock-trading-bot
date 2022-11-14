ec0b16e9755d14b61e842402cf1b03a63093625e
Josh <josh@hessjd.com>
2022-11-14 16:56:47 -0600

    update styles

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -3,6 +3,7 @@ import { Link, useNavigate } from 'react-router-dom';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import axios from 'axios';
+import '../App.css';
 
 function Login() {
 	const navigate = useNavigate();
---
+++ server/controllers/userController.js
@@ -0,0 +1,40 @@
+const User = require('../models/user');
+const bcrypt = require('bcrypt');
+const async = require('async');
+
+module.exports.register = async (req, res, next) => {
+	console.log('Register');
+	const { username, password } = req.body;
+	const usernameCheck = await User.findOne({ username });
+	if (usernameCheck) {
+		return res.json({ msg: 'Username Taken', status: false });
+	}
+	const hashedPassword = await bcrypt.hash(password, 10);
+	const user = User.create({
+		username,
+		password: hashedPassword,
+	});
+	delete user.password;
+	return res.json({ status: true, user });
+};
+
+module.exports.login = async (req, res, next) => {
+	const { username, password } = req.body;
+	const user = await User.findOne({ username });
+	if (!user) {
+		return res.json({
+			msg: 'Incorrect username or password',
+			status: false,
+		});
+	}
+	const passwordCheck = await bcrypt.compare(password, user.password);
+	if (!passwordCheck) {
+		return res.json({
+			msg: 'Incorrect username or password',
+			status: false,
+		});
+	}
+	user.password = undefined;
+	console.log('Login successful');
+	return res.json({ msg: 'Login successful', status: true, user });
+};
